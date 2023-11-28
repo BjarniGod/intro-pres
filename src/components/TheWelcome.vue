02a3adab9ccd2c4c0c4dcdce586ba1f274370578
Bjarni Jonsson <89957113+BjarniGod@users.noreply.github.com>
2023-11-28 18:29:08 +0000

    Update full screen cards on click

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="flashcard-app" class="container">
-
-<h1>Bjarni Guðmann Jónsson</h1>
-
-<ul class="flashcard-list">
-  <li v-on:click="toggleCard(card)" v-for="(card, index) in cards">
-    <transition name="flip">
-      <p v-bind:key="card.flipped" class="card">
-          {{ card.flipped ? card.back : card.front }}
-      </p>
-    </transition>
-  </li>
-</ul>
-</div>
+    <h1>Bjarni Guðmann Jónsson</h1>
+    <ul class="flashcard-list">
+      <li v-for="(card, index) in cards" :key="index" v-on:click="toggleCard(card)">
+        <transition name="flip">
+          <div v-if="card.flipped" class="card back full-screen" :class="{ 'is-expanded': card.isExpanded }">{{ card.back }}</div>
+          <div v-else class="card front" :style="{ backgroundImage: 'url(' + card.imageUrl + ')' }"></div>
+        </transition>
+      </li>
+    </ul>
+  </div>
 </template>
 
+
+
+
 <script>
-import { Flipper, Flipped } from "vue-flip-toolkit";
+import { VueFlip } from 'vue-flip';
 
 export default {
   components: {
-    Flipped,
-    Flipper
+    'vue-flip': VueFlip
   },
   data() {
     return {
@@ -33,8 +32,11 @@ export default {
   },
   methods: {
     toggleCard(card) {
-      card.flipped = !card.flipped;
-    },
+    card.flipped = !card.flipped;
+    this.$nextTick(() => {
+      card.isExpanded = card.flipped; // Expand the card if it is flipped
+    });
+  },
     addNew() {
       if (!this.newFront || !this.newBack) {
         this.error = true;
@@ -49,225 +51,71 @@ export default {
         this.error = false;
       }
     },
-    deleteCard(index) {
-      this.cards.splice(index, 1);
-    }
   }
 };
 
 const cards = [
     {
       front: 'Portrait',
-      back: 'Information about me!',
+      back: '',
       flipped: false,
+      imageUrl: 'src/assets/img/portrait.jpg',
     },
     {
       front: 'Family',
       back: 'Information about my family!',
       flipped: false,
+      imageUrl: 'assets/img/',
   
     },
     {
       front: 'Iceland',
       back: 'Information about Iceland!',
       flipped: false,
+      imageUrl: 'assets/img/',
     },
     {
       front: 'Basketball',
       back: 'Information about basketball!',
       flipped: false,
+      imageUrl: 'assets/img/',
     },
     {
       front: 'Home',
       back: 'Information about home!',
       flipped: false,
+      imageUrl: 'assets/img/',
     },
     {
       front: 'Travels',
       back: 'Information about my travels!',
       flipped: false,
+      imageUrl: 'assets/img/',
     },
     {
       front: 'Hobbies',
       back: 'Information about my hobbies!',
       flipped: false,
+      imageUrl: 'assets/img/',
     },
     {
       front: 'Basketball',
       back: 'Information about basketball!',
       flipped: false,
+      imageUrl: 'assets/img/',
     },
     {
       front: 'Basketball',
       back: 'Information about basketball!',
       flipped: false,
+      imageUrl: 'assets/img/',
     },
     {
       front: 'Basketball',
       back: 'Information about basketball!',
       flipped: false,
+      imageUrl: 'assets/img/',
     },
 ]; 
 </script>
-
-
-<!-- <style>
-body {
-  font-family: 'Montserrat', sans-serif;
-  text-align: center;
-  background: rgb(9, 4, 33);
-  margin: 0;
-  /* height: 100vh; */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-h1 {
-  color: white;
-}
-
-.container {
-  max-width: 800px;
-  padding: 2em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-ul {
-  padding-left: 0;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  width: 100%; 
-}
-
-li {
-  list-style-type: none;
-  padding: 10px 10px;
-  transition: all 0.3s ease;
-}
-
-.card {
-  display: block;
-  width: 150px;
-  height: 175px;
-  padding: 80px 50px;
-  background-color: #51aae5;
-  border-radius: 7px;
-  margin: 5px;
-  text-align: center;
-  line-height: 27px;
-  cursor: pointer;
-  position: relative;
-  color: #fff;
-  font-weight: 600;
-  font-size: 20px;
-  /* -webkit-box-shadow: 9px 10px 22px -8px rgba(209,193,209,.5);
-  -moz-box-shadow: 9px 10px 22px -8px rgba(209,193,209,.5);
-  box-shadow: 9px 10px 22px -8px rgba(255, 255, 255, 0.5); */
-  will-change: transform;
-}
-
-li:hover{
-  transform: scale(1.1);
-}
-
-li:nth-child(-n+3) .card{
-  background-color: #e65f51;
-  }
-
-li:nth-child(2n+1) .card{
-  background-color: #a17de9;
-  }
-
-li:nth-child(4n) .card{
-  background-color: #feca34;
-  }
-
-li:nth-child(5n-2) .card{
-  background-color: #51aae5;
-  }
-
-li:nth-child(4n+4) .card{
-  background-color: #feca34;
-  }
-
-li:nth-child(-7n+7) .card{
-  background-color: #e46055;
-  }
-
-.delete-card {
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 10px 15px;
-  opacity: .4;
-  transition: all 0.5s ease;
-}
-
-.delete-card:hover, .error {
-  opacity: 1;
-  transform: rotate(360deg);
-}
-
-.flip-enter-active {
-  transition: all 0.4s ease;
-}
-
-.flip-leave-active {
-  display: none;
-}
-
-.flip-enter, .flip-leave {
-  transform: rotateY(180deg);
-  opacity: 0;
-
-}
-
-/* Form */
-.flashcard-form{
-  position: relative;
-}
-
-
-label{
-  font-weight: 400;
-  color: #333;
-  margin-right: 10px;
-}
-
-input{
-  border-radius: 5px;
-  border: 2px solid #eaeaea;
-  padding: 10px;
-  outline: none;
-}
-
-button{
-  border-radius: 5px;
-  border: 1px solid #87cb84;
-  background-color: #87cb84;
-  padding: 8px 15px;
-  outline: none;
-  font-size: 14px;
-  font-weight: 700;
-  color: #fff;
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
-
-button:hover{
-  background-color: #70a66f;
-}
-
-.error{
-  margin-top: 10px;
-  display: block;
-  color: #e44e42;
-  font-weight: 600;
-}
-
-
-</style> -->
+ç
